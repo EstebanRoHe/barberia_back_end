d0f8d17a9a775ad693f527aa16fdd0d13a784f24
EstebanRoHe <esteban_rojashe@hotmail.com>
2023-07-24 22:35:14 -0600

    update cors
    update cors

--- barberia_api/settings.py
+++ barberia_api/settings.py
@@ -118,8 +118,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://barberia-gamma.vercel.app/",
+    "https://barberia-gamma.vercel.app",
 ]
 
 
